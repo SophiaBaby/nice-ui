2698062fd7e2018e28ff3eb1745528b74c8be003
zhangxin <1759553621@qq.com>
2019-09-24 17:44:39 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import niceUI from ‘./components’
 
+
+// Vue.use(niceUI)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/packages/Button/src/main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是button组件</div>
+</template>
+
+<script>
+  export default {
+    name: "main"
+  }
+</script>
+
+<style scoped>
+
+</style>
